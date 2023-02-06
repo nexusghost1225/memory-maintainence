08747db5440cf06162c4aafec6d5ebf01ca17b58
nexusghost1225 <JGBxmas2000@hotmail.com>
2023-02-05 22:23:00 -0500

    form initiated

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css";
 
-function App() {
+const App = () => {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("You clicked submit")
+  }
+  const [value, setValue] = React.useState('');
+
+  const handleChange = (event) => {
+
+    setValue(event.target.value);
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+
+      <label>
+
+        <h1 className="Question"> How valuable is your memory? </h1>
+
+        <section className="memory">
+          <input
+            placeholder="Place your memories in my hands"
+            style={{ width: "200px" }}
+            onChange={() => { }}
+          
+          />
+        </section>
+        <section>
+        <form onSubmit={handleSubmit}>
+          <button onClick={() => { }}>Place </button>
+        </form>
+        </section>
+
+        <div className="input">
+          <select value={value} onChange={handleChange}>
+
+            <option value="Extremely Important">Extremely important</option>
+
+            <option value="Important">Important</option>
+
+            <option value="I Would like to remember">I Would like to remember</option>
+
+          </select>
+        </div>
+      </label>
+
+      <p className="choice"> {value}</p>
+
     </div>
+
+
+
+
   );
-}
+
+};
 
 export default App;
+
+
+
