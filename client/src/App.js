2a31352614ca79680fc705cf5dba7ba375a5886d
nexusghost1225 <JGBxmas2000@hotmail.com>
2023-02-06 20:09:03 -0500

    client finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,67 +1,71 @@
 import React from "react"
 import "./App.css";
 
+
+
+
+
+
+
+
 const App = () => {
+
 
   function handleSubmit(e) {
     e.preventDefault();
     console.log("You clicked submit")
-  }
-  const [value, setValue] = React.useState('');
-
-  const handleChange = (event) => {
-
-    setValue(event.target.value);
-
   };
+
+
+  const submitMemory = () => {
+    alert("Memory Cast");
+  }
+
+
 
   return (
 
     <div>
 
-      <label>
+      {/* <label> */}
 
-        <h1 className="Question"> How valuable is your memory? </h1>
+
+      <section className="place">
+        <form>
+          <h1 className="Question"> Cast your memories </h1>
+
+          <section className="memory">
+            <input
+              placeholder="Place your memories in my hands"
+              style={{ width: "200px" }}
+              onChange
+
+            />
+          </section>
+        </form>
+      </section>
+
+
+
+
+
+
+
+      <div onSubmit={handleSubmit}>
+      
 
         <section className="memory">
-          <input
-            placeholder="Place your memories in my hands"
-            style={{ width: "200px" }}
-            onChange={() => { }}
-          
-          />
+        <button onClick={submitMemory} style={{ width: "100px" }} type="submit">Place </button>
+
         </section>
-        <section>
-        <form onSubmit={handleSubmit}>
-          <button onClick={() => { }}>Place </button>
-        </form>
-        </section>
+      </div>
+      
+      </div>
+);
 
-        <div className="input">
-          <select value={value} onChange={handleChange}>
+}
 
-            <option value="Extremely Important">Extremely important</option>
-
-            <option value="Important">Important</option>
-
-            <option value="I Would like to remember">I Would like to remember</option>
-
-          </select>
-        </div>
-      </label>
-
-      <p className="choice"> {value}</p>
-
-    </div>
-
-
-
-
-  );
-
-};
-
-export default App;
+      export default App;
 
 
 
